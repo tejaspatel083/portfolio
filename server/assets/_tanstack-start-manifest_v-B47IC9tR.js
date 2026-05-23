8a2901e978ffdef715f019e1fd7e953ba734303a
Tejas Patel <tejaspatel@macbookpro.home>
2026-05-23 01:46:06 -0400

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-B47IC9tR.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/tejaspatel/Documents/Resume/Portfolio/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-8eauSHb1.js"] }, "/": { filePath: "/Users/tejaspatel/Documents/Resume/Portfolio/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-Bnz1lKmu.js"] } }, clientEntry: "/assets/index-8eauSHb1.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-B_y1VEMp.js";
+import "node:events";
+export {
+  w as default
+};
